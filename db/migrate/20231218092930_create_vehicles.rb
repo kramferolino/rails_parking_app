bd9fcf13b3b7064bf475d9556eb5e42440cf53c6
Mark Ferolino <kramferolino@gmail.com>
2023-12-22 19:51:15 +0800

    Added the correct migration file

--- db/migrate/20231218092930_create_vehicles.rb
+++
@@ -1,9 +0,0 @@
-class CreateVehicles < ActiveRecord::Migration[7.1]
-  def change
-    create_table :vehicles do |t|
-      t.integer :size, default: 0
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20231218133950_add_parking_slot_id_to_vehicles.rb
+++
@@ -1,5 +0,0 @@
-class AddParkingSlotIdToVehicles < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :vehicles, :parking_slot, null: false, foreign_key: true
-  end
-end
